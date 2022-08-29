1df935cbd66a6c2ab3c3e1eedc7cb764674dfce9
Adam Folta <afolta@thecitybase.com>
2022-08-28 20:58:26 -0400

    add initial trips controller

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -1,7 +1,29 @@
 class Api::V1::TripsController < ApplicationController
   def index
+    trips = Trip.all 
+    render json: trips, status: 200
   end
 
   def show
+    trip = Trip.find_by(id: params[:id])
+    if trip
+      render json: product, status: 200
+    else
+      render json {error: "Trip not found."}
+    end
   end
+
+  private
+    def trip_params
+      params.require(:trip).permit([
+        :bike_type,
+        :bike_id,
+        :duration,
+        :start_date,
+        :end_date,
+        :start_station,
+        :end_station
+      ])
 end
+
+
