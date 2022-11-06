6810938ba8446f3f2b4d1f618a52e6f73e7eb280
Adam Folta <afolta@thecitybase.com>
2022-11-06 13:00:21 -0500

    remove trip comments

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,8 +2,5 @@
 
 class Trip < ApplicationRecord
   has_many :trip_dates
-  # belongs_to :station
   has_many :stations
-  # belongs_to :start_station, class_name: "Station", foreign_key: "station_id"
-  # belongs_to :end_station, class_name: "Station", foreign_key: "station_id"
 end
