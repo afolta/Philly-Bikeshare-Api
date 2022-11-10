0686c3317018fe37a8e177b76cc02aadb795203f
Adam Folta <afolta@thecitybase.com>
2022-11-09 21:25:45 -0500

    port station model associations to json

--- app/resources/trip_resource.rb
+++ app/resources/trip_resource.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class TripResource < JSONAPI::Resource
-  attributes :bike_type, :bike_id, :duration, :start_station,
-             :end_station, :start_time, :end_time
+  attributes :bike_type, :bike_id, :duration, :start_station_id,
+             :end_station_id, :start_time, :end_time
 
   has_many :trip_dates
+  belongs_to :start_station, class_name: 'Station', foreign_key: 'start_station_id'
+  belongs_to :end_station, class_name: 'Station', foreign_key: 'end_station_id'
 end
