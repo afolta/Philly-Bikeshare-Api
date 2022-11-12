c7708f7e87ecb0658077e574c922f008a6fdf2db
Adam Folta <afolta@thecitybase.com>
2022-11-12 14:09:53 -0500

    add trips by start_station

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -10,6 +10,14 @@ class TripsController < JSONAPI::ResourceController
 
   def start_station
     trip = Trip.joins("INNER JOIN stations ON stations.id = trips.start_station_id WHERE stations.station_name='#{params[:start_station]}'")
-    render json: trip
+  end
+
+  def trips_by_start_station
+    render json: start_station
+  end
+
+  def count
+    count = start_station.count
+    render json: { count: count }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@
 
 Rails.application.routes.draw do
   jsonapi_resources :trips
-  get '/trips/station/:start_station' => 'trips#start_station'
   get '/stations' => 'stations#index'
   get 'stations/:id' => 'stations#show'
+  get '/trips/station/:start_station' => 'trips#trips_by_start_station'
+  get '/trips/count/:start_station' => 'trips#count'
 end
