d01c94c0c0e70b66f223d328469bb63832d7de2d
Adam Folta <afolta@thecitybase.com>
2022-11-13 18:01:10 -0500

    Add filter by date

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -26,7 +26,15 @@ class TripsController < JSONAPI::ResourceController
     # trip = Trip.last.start_time.strftime('%d-%m-%y')
     # date_input = Time.parse(params[:date])
     # render json: date_input
-    trip = Trip.where('extract(year from start_time) = ?', params[:date])
+    # trip = Trip.where('start_time.strftime('%d-%m-%y') = ?', params[:date])
+    # trip = Trip.where('extract(year from start_time) = ?', params[:date])
+    # render json: trip
+    variable = params[:date].split('-')
+
+    date = Date.new(variable[0].to_i, variable[1].to_i, variable[2].to_i)
+
+    trip = Trip.where(start_time: date.all_day)
+
     render json: trip
   end
 end
